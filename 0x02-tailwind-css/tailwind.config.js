aea5e65f7d86696a1789f286d247884a5b9cfa90
elorm gershon <alidugershon@gmail.com>
2025-07-03 05:56:37 +0000

    Complex Grid Layout with Nested Grids

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,6 +1,17 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./src/**/*.{html,js}", "./1-index.html, ./2-index.html"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./1-index.html"],
+  content: [
+    "./src/**/*.{html,js}", // include everything in src/
+    "./*.html"              // ✅ include all HTML files in root folder
+  ],
   theme: {
     extend: {},
   },
